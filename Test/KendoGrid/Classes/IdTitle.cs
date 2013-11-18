5f9072eb79ad6f27626bb5294f3c76e598916d38
Anatoliy <ryecatcher@mail.ru>
2013-11-18 20:52:18 +0200

    conflict resolved

--- Test/KendoGrid/Classes/IdTitle.cs
+++ Test/KendoGrid/Classes/IdTitle.cs
@@ -9,10 +9,6 @@ namespace KendoGrid
     {
         public int Id { get; set; }
         public string Title { get; set; }
-<<<<<<< HEAD
         public string Description { get; set; }
-=======
-        public long Description { get { return Title.Length; } }
->>>>>>> origin/131118_Sergey_Test1
     }
 }
