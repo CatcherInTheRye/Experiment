570a3f3f64b286719a3cf85f5e4c881c0d26e2ff
SerhiiKoza4enko <sergey.koza4enko@gmail.com>
2013-11-24 14:43:50 +0200

    Add Description class

--- Test/KendoGrid/Classes/IdTitle.cs
+++ Test/KendoGrid/Classes/IdTitle.cs
@@ -11,4 +11,10 @@ namespace KendoGrid
         public string Title { get; set; }
         public string Description { get; set; }
     }
+
+    public class DescriptionTitle
+    {
+        public IdTitle Id { get; set; }
+        public string Description { get; set; }
+    }
 }
